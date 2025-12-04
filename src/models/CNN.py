e148da9717dc9a3899af7541e1eef1ea9e7ffbb5
Valerio-git <valeriodambra03@gmail.com>
2025-12-04 21:08:53 +0100

    updated CNN with 3rd conv layer, branc norm, dropout, adaptive avg pooling (gap)

--- src/models/CNN.py
+++ src/models/CNN.py
@@ -1,24 +1,45 @@
 import torch.nn as nn
 
 class CNN(nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
+    def _init_(self, num_classes):
+        super()._init_()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * 56 * 56, 512)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.fc1 = nn.Linear(128, 512)
+        self.dropout = nn.Dropout(p=0.5)
         self.fc2 = nn.Linear(512, num_classes)
     
     def forward(self, x):
         x = self.conv1(x)
+        x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
+
         x = self.conv2(x)
+        x = self.bn2(x)
         x = self.relu(x)
         x = self.maxpool(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.gap(x)
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
