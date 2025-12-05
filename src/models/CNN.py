101f5fe64821225d4f4413116f0da3543242705d
TommasoSbrenna <sbrenna.2089725@studenti.uniroma1.it>
2025-12-05 15:09:38 +0100

    modified CNN

--- src/models/CNN.py
+++ src/models/CNN.py
@@ -1,8 +1,8 @@
 import torch.nn as nn
 
 class CNN(nn.Module):
-    def _init_(self, num_classes):
-        super()._init_()
+    def __init__(self, num_classes):
+        super().__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
         
