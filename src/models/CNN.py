9d4d02729835055f47e7125206d1aded906c3d70
TommasoSbrenna <sbrenna.2089725@studenti.uniroma1.it>
2025-12-09 14:58:34 +0100

    Modified CNN and created fine_tuning folder

--- src/models/CNN.py
+++ src/models/CNN.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 class CNN(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
+        # ----- Convolutional layers -----
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
         
@@ -16,30 +17,49 @@ class CNN(nn.Module):
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.bn3 = nn.BatchNorm2d(128)
 
+        # ----- Fully connected -----
+        # 128 -> 512 -> 256 -> num_classes
         self.fc1 = nn.Linear(128, 512)
+        self.bn_fc1 = nn.BatchNorm1d(512)
+
+        self.fc2 = nn.Linear(512, 256)
+        self.bn_fc2 = nn.BatchNorm1d(256)
+
         self.dropout = nn.Dropout(p=0.5)
-        self.fc2 = nn.Linear(512, num_classes)
+        self.fc3 = nn.Linear(256, num_classes)
     
     def forward(self, x):
+        # ----- Conv block 1 -----
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
 
+        # ----- Conv block 2 -----
         x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu(x)
         x = self.maxpool(x)
 
+        # ----- Conv block 3 -----
         x = self.conv3(x)
         x = self.bn3(x)
         x = self.relu(x)
         x = self.maxpool(x)
 
+        # Global Average Pooling
         x = self.gap(x)
         x = x.view(x.size(0), -1)
+
+        # ----- Fully connected -----
         x = self.fc1(x)
+        x = self.bn_fc1(x)
         x = self.relu(x)
-        x = self.dropout(x)
+
         x = self.fc2(x)
+        x = self.bn_fc2(x)
+        x = self.relu(x)
+
+        x = self.dropout(x)
+        x = self.fc3(x)
         return x
