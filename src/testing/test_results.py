b065ec54ae795f2dd8431e31607f608e56d1ecd6
Valerio-git <valeriodambra03@gmail.com>
2025-12-11 17:29:35 +0100

    removed test_fine_tune and test_results

--- src/testing/test_results.py
+++
@@ -1,6 +0,0 @@
-from src.utils.functions import ask_model_type_from_console
-from src.testing.test import main
-
-if __name__ == "__main__":
-    model_type, white = ask_model_type_from_console()
-    main()
